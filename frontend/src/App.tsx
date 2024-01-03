ffff0b1246b3ff4a289f8b10441204798a23a3fc
Tahsin Hasan <tahsinhasan2@gmail.com>
2024-01-03 13:52:35 -0800

    Sign In Form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,13 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
-// Note, add 2 checks in Username and Page, if the Username or Page is invalid, give the NotFound page.
+/* Notes: Add 2 checks in Username and Page, if the Username or Page is invalid, give the NotFound page.
+          Access the Username from the url, for both the page and the literal page.
+          Check if the Page - Questions has been broken due to the username modification.
+          Alter the List of Pages to include the Username. Perhaps in the backend, create a new object called Pages.
+*/
+
+/* Questions: Should I add in Tailwind CSS to make it easier to copy in my sign in and sign up pages? I don't really use it. */
 function App() {
   return (
     <>
